8eab39232dc7120d4a3657dd73486d9c7fcc024f
ThiagoVini-S <th.vinialmeida@gmail.com>
2024-12-17 11:41:26 -0300

    Feat: Adicionando rodape

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Topo from './Components/Topo';
 import Bstart from './Components/BodyStart';
 import Projetos from './Components/Projetos';
-
+import Rodape from './Components/Rodape';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Topo/>
       <Bstart/>
       <Projetos/>
-
+      <Rodape/>
     </>
   );
 }
---
+++ src/Components/Rodape.js
@@ -0,0 +1,39 @@
+import { FaGithubSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Rodape(){
+
+    return(
+        <>
+            <footer className="bg-neutral-900 flex flex-wrap justify-between">
+                <h3 className="text-center font-extrabold text-4xl w-full p-14 text-violet-400">/ Contato</h3>
+                    <div className="w-[280px]">
+                        <p className="text-center text-violet-400 text-2xl">Ajude um dev iniciante!</p>
+                        qrcode
+                    </div>
+                <nav className="">
+                    <div className="mb-5">
+                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-orange-400"><FaGithubSquare className="inline"/>GitHub</a>
+                    </div>
+                    <div className="mb-5">
+                        <a href="#" className="text-xl p-2   hover:text-sky-400 sm:text-5xl"><FaSquareXTwitter className="inline"/>Twitter</a>
+                    </div>
+                    <div className="mb-5">
+                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-green-500"><FaWhatsappSquare className="inline"/>Whatsapp</a>
+                    </div>
+                    <div className="mb-5">
+                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-red-300"><FaInstagramSquare className="inline"/>
+                        Instagram</a>
+                    </div>
+                    <div className="mb-5">
+                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-blue-600"><FaLinkedin className="inline"/>
+                        LinkedIn</a>
+                    </div>
+                </nav>
+            </footer>
+        </>
+    )
+}
