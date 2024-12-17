b46472159b0b22e6eae08449a5e67a3dacfe4ce7
ThiagoVini-S <th.vinialmeida@gmail.com>
2024-12-17 11:38:59 -0300

    Feat: sessão de projetos e habilidades

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Topo from './Components/Topo';
 import Bstart from './Components/BodyStart';
+import Projetos from './Components/Projetos';
+
 
 function App() {
   return (
     <>
       <Topo/>
       <Bstart/>
-      
+      <Projetos/>
+
     </>
   );
 }
---
+++ src/Components/Projetos.js
@@ -0,0 +1,54 @@
+export default function Projetos(){
+    return(
+        <>
+            <section className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+            <h3 className="mt-40 text-violet-400 col-span-2 sm:col-span-4 text-3xl pl-5"> == Habilidades</h3>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">Linguagens</h4>
+                    <p>JavaScript</p>
+                    <p>C#</p>
+                </div>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">Ferramentas</h4>
+                    <p>VSCode</p>
+                    <p>Gimp</p>
+                    <p>Figma</p>
+                </div>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">Frameworks</h4>
+                    <p>React</p>
+                </div>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">Outros</h4>
+                    <p>HTML5</p>
+                    <p>CSS3</p>
+                </div>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">Bibliotecas</h4>
+                    <p>TailWind</p>
+                    <p>Bootstrap</p>
+                </div>
+            </section>
+            <section className="mt-40 grid grid-cols-2 gap-5 sm:grid-cols-4">
+                <h3 className="mt-40 col-span-2 sm:col-span-4 text-violet-400 text-3xl pl-5">== Trabalhos</h3>
+                <Trabalhos/>
+                <Trabalhos/>
+                <Trabalhos/>
+                <Trabalhos/>
+                <Trabalhos/>
+                <Trabalhos/>
+            </section>
+        </>
+    )
+}
+
+function Trabalhos(){
+    return (
+        <>
+                <div className="border-2 border-zinc-600 text-center m-5">
+                    <h4 className="border-b-2 border-zinc-600 font-extrabold">XXXXX</h4>
+                    <p className="text-yellow-200">Em breve</p> 
+                </div>
+        </>
+    )
+}
