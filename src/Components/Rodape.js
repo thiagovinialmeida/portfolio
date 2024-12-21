9fa058cb4b39c1a9b017aeba6fa08643ac5234a0
ThiagoVini-S <th.vinialmeida@gmail.com>
2024-12-20 22:51:50 -0300

    Fix: Finalização do footer mobile

--- src/Components/Rodape.js
+++ src/Components/Rodape.js
@@ -10,31 +10,29 @@ export default function Rodape(){
 
     return(
         <>
-            <footer className="bg-neutral-900 flex flex-wrap justify-between">
+            <footer className="bg-neutral-900 flex flex-wrap">
                 <h3 className="text-center font-extrabold text-4xl w-full p-14 text-violet-400">/ Contato</h3>
-                    <div className="w-[280px]">
-                        <p className="text-center text-violet-400 text-2xl">Ajude um dev iniciante!</p>
-                        <img src={Qrcode} className="w-6/12"/>
-                    </div>
-                <nav className="">
-                    <div className="mb-5">
-                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-orange-400"><FaGithubSquare className="inline"/>GitHub</a>
-                    </div>
-                    <div className="mb-5">
-                        <a href="#" className="text-xl p-2   hover:text-sky-400 sm:text-5xl"><FaSquareXTwitter className="inline"/>Twitter</a>
-                    </div>
-                    <div className="mb-5">
-                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-green-500"><FaWhatsappSquare className="inline"/>Whatsapp</a>
-                    </div>
-                    <div className="mb-5">
-                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-red-300"><FaInstagramSquare className="inline"/>
-                        Instagram</a>
-                    </div>
-                    <div className="mb-5">
-                        <a href="#" className="text-xl sm:text-5xl p-2 hover:text-blue-600"><FaLinkedin className="inline"/>
-                        LinkedIn</a>
-                    </div>
+                <nav className="w-full mb-16">
+                        <div className="w-full py-3 border-b-4 border-y-zinc-700">
+                            <a href="#" className="text-xl sm:text-5xl p-2 hover:text-orange-400 pl-7"><FaGithubSquare className="inline"/> GitHub</a>
+                        </div>
+                        <div className="w-full py-3 border-b-4 border-y-zinc-700">
+                            <a href="#" className="text-xl p-2 hover:text-sky-400 sm:text-5xl pl-7"><FaSquareXTwitter className="inline"/> Twitter</a>
+                        </div>
+                        <div className="w-full py-3 border-b-4 border-y-zinc-700">
+                            <a href="#" className="text-xl sm:text-5xl p-2 hover:text-green-500 pl-7"><FaWhatsappSquare className="inline"/> Whatsapp</a>
+                        </div>
+                        <div className="w-full py-3 border-b-4 border-y-zinc-700">
+                            <a href="#" className="text-xl sm:text-5xl p-2 hover:text-red-300 pl-7"><FaInstagramSquare className="inline"/> Instagram</a>
+                        </div>
+                        <div className="w-full py-3">
+                            <a href="#" className="text-xl sm:text-5xl p-2 hover:text-blue-600 pl-7"><FaLinkedin className="inline"/> LinkedIn</a>
+                        </div>
                 </nav>
+                <div className="w-full">
+                        <p className="text-center text-violet-400 text-2xl">Área Pix</p>
+                        <img src={Qrcode} className="w-6/12 m-auto pt-5"/>
+                </div>
             </footer>
         </>
     )
