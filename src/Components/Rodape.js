c5625dd331b33e8145a6f24202517fc73ba6cf77
ThiagoVini-S <th.vinialmeida@gmail.com>
2024-12-25 00:58:45 -0300

    Fix: Atualização do rodapé

--- src/Components/Rodape.js
+++ src/Components/Rodape.js
@@ -1,8 +1,5 @@
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaWhatsappSquare, FaInstagramSquare, FaLinkedin} from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaWhatsappSquare } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
 
 import Qrcode from "../Assets/Qrcode.jpg"
 
