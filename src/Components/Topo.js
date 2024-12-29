b0e7e0b11a155117ee5aea5f903e5b1591a1136c
ThiagoVini-S <th.vinialmeida@gmail.com>
2024-12-29 16:16:34 -0300

    Fixação do topo

--- src/Components/Topo.js
+++ src/Components/Topo.js
@@ -19,25 +19,25 @@ export default function Topo(){
    
     return(
         <>
-        <header>
+        <header  className="fixed top-0 left-0 right-0 w-full">
             <nav>
-                <div className="flex justify-between">
-                    <h1>THDev</h1>
-                    <RxHamburgerMenu id="burger-menu" className="text-4xl botao sm:hidden" onClick={mostrarModo}/>
-                </div>
-                <div className="gap-5 max-sm:hidden">
-                    <a href="#Home">Home</a>
-                    <a href="#Sobremim">Sobre mim</a>
-                    <a href="#Trabalhos">Trabalhos</a>
-                    <a href="#Contato">Contato</a>
+                <div className="bg-neutral-900 flex flex-wrap justify-between">
+                        <h1 className="text-violet-400 text-2xl sm:pl-10 p-2">THDev</h1>
+                        <RxHamburgerMenu id="burger-menu" className="text-4xl m-2 sm:hidden" onClick={mostrarModo}/>
+                    <div className="flex gap-10 p-2 max-sm:hidden">
+                        <a href="#Home" className="hover:bg-violet-400 p-2 rounded-lg">Home</a>
+                        <a href="#Sobremim" className="hover:bg-violet-400 p-2 rounded-lg">Sobre mim</a>
+                        <a href="#Trabalhos" className="hover:bg-violet-400 p-2 rounded-lg">Trabalhos</a>
+                        <a href="#Contato" className="mr-10 hover:bg-violet-400 p-2 rounded-lg">Contato</a>
+                    </div>
                 </div>
             </nav>
             <div id="mobileMenu" className={displayMode}>
                 <nav className="flex flex-col grow-0 content-stretch text-center w-full h-screen sm:hidden">
-                    <a href="#Home" className="border-b-4 border-t-8 border-y-zinc-700 p-4">Home</a>
-                    <a href="#Sobremim" className="border-y-4 border-y-zinc-700 p-4">Sobre mim</a>
-                    <a href="#Trabalhos" className="border-y-4 border-y-zinc-700 p-4">Trabalhos</a>
-                    <a href="#Contato" className="border-y-4 border-b-8 border-y-zinc-700  p-4">Contato</a>
+                    <a href="#Home" className="bg-neutral-900 border-b-2 border-t-4 border-y-zinc-700 p-4">Home</a>
+                    <a href="#Sobremim" className="bg-neutral-900 border-y-2 border-y-zinc-700 p-4">Sobre mim</a>
+                    <a href="#Trabalhos" className="bg-neutral-900 border-y-2 border-y-zinc-700 p-4">Trabalhos</a>
+                    <a href="#Contato" className="bg-neutral-900 border-y-2 border-b-4 border-y-zinc-700  p-4">Contato</a>
                 </nav>
             </div>
         </header>
